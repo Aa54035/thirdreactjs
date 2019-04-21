7af25878ff61cf7942e463ecb0ece9e1fea61291
ashish <aa54035@yahoo.com>
2019-04-21 18:12:17 +0530

    h3 footer added

--- thirdreactjs/Footer.js
+++ thirdreactjs/Footer.js
@@ -1,3 +1,5 @@
 <div>
     <h1> this is for footer </h1>
+<h2>footer nwe added </h2>
+<h3>h3 footer added </h3>
 </div>
--- thirdreactjs/index.js
+++
@@ -1,4 +0,0 @@
-<div>
-
-    <h1> hellow every one how are your </h1>
-</div>
